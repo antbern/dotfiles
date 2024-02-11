f46be4a47b58b4ed8e6054d37de36bd5248848af
antbern <40672068+antbern@users.noreply.github.com>
2024-02-11 21:55:11 +0100

    [nvim] Improve telescope keybindings, install dressing.nvim

--- config/nvim/lua/antbern/plugins/telescope.lua
+++ config/nvim/lua/antbern/plugins/telescope.lua
@@ -1,21 +1,49 @@
 return {
 	'nvim-telescope/telescope.nvim',
-	tag = '0.1.5',
-	-- or                              , branch = '0.1.x',
-	dependencies = { 'nvim-lua/plenary.nvim' },
-
+	branch = '0.1.x',
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		'nvim-tree/nvim-web-devicons',
+	},
 	config = function()
+		local telescope = require('telescope')
 		local builtin = require('telescope.builtin')
-		-- TODO: add live grep builtin.live_grep()
-		vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-		vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-		vim.keymap.set('n', '<leader>ps', function()
-			-- Note: requires `ripgrep` to be installed
-			builtin.grep_string({ search = vim.fn.input("Grep > ") });
-		end)
+		local actions = require('telescope.actions')
 
-		vim.keymap.set('n', '<leader>lg', builtin.live_grep, {})
-		vim.keymap.set('n', '<leader>of', builtin.oldfiles, {})
-		vim.keymap.set('n', '<leader>fg', builtin.current_buffer_fuzzy_find, {})
+		telescope.setup({
+			defaults = {
+				mappings = {
+					i = {
+						["<C-k>"] = actions.move_selection_previous, -- move to prev result
+						["<C-j>"] = actions.move_selection_next, -- move to next result
+						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+					}
+				}
+			}
+		})
+
+		telescope.load_extension("fzf")
+
+		-- set keymaps
+		local keymap = vim.keymap -- for conciseness
+
+		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+
+
+		-- TODO: add live grep builtin.live_grep()
+		-- vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+		-- vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+		-- vim.keymap.set('n', '<leader>ps', function()
+		-- 	-- Note: requires `ripgrep` to be installed
+		-- 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+		-- end)
+
+		-- vim.keymap.set('n', '<leader>lg', builtin.live_grep, {})
+		-- vim.keymap.set('n', '<leader>of', builtin.oldfiles, {})
+		vim.keymap.set('n', '<leader>fb', builtin.current_buffer_fuzzy_find, { desc = "Fuzzy find in current buffer" })
 	end
 }
