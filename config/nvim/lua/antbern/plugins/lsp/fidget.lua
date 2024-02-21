9da5b38cef82282e40165bd87fcf4bb2ed153949
antbern <40672068+antbern@users.noreply.github.com>
2024-02-21 22:10:44 +0100

    [nvim] increase nvim lsp buffer size to not drop any messages

--- config/nvim/lua/antbern/plugins/lsp/fidget.lua
+++ config/nvim/lua/antbern/plugins/lsp/fidget.lua
@@ -1,6 +1,13 @@
-	return {
+return {
 	"j-hui/fidget.nvim",
 	opts = {
 		-- options
+		progress = {
+			-- Options related to Neovim's built-in LSP client
+			lsp = {
+				-- See: https://github.com/j-hui/fidget.nvim/issues/167 for explanation
+				progress_ringbuf_size = 2048, -- Configure the nvim's LSP progress ring buffer size to make rust_analyzer updates work correctly
+			},
+		},
 	},
 }
