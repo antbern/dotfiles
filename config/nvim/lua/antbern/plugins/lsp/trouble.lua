4fd90e5a1804c03c57a1f4255a6caa7c106f5049
antbern <40672068+antbern@users.noreply.github.com>
2024-02-24 20:37:17 +0100

    [nvim] add `trouble.nvim` for global LSP diagnostics

---
+++ config/nvim/lua/antbern/plugins/lsp/trouble.lua
@@ -0,0 +1,20 @@
+return {
+	"folke/trouble.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		}
+		local trouble = require("trouble")
+		trouble.setup(opts)
+		-- setup keymap
+		vim.keymap.set("n", "<leader>xx", function() trouble.toggle() end)
+		vim.keymap.set("n", "<leader>xw", function() trouble.toggle("workspace_diagnostics") end)
+		vim.keymap.set("n", "<leader>xd", function() trouble.toggle("document_diagnostics") end)
+		vim.keymap.set("n", "<leader>xq", function() trouble.toggle("quickfix") end)
+		vim.keymap.set("n", "<leader>xl", function() trouble.toggle("loclist") end)
+		-- vim.keymap.set("n", "gR", function() trouble.toggle("lsp_references") end)
+	end
+}
