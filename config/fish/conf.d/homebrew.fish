6bcce3ae8eb24ce2a7ede230d0bcbf87aac36ec9
antbern <40672068+antbern@users.noreply.github.com>
2024-10-11 10:18:57 +0200

    [fish] prepend brew path instead of post-pend

--- config/fish/conf.d/homebrew.fish
+++ config/fish/conf.d/homebrew.fish
@@ -2,8 +2,8 @@ if test -e /home/linuxbrew/.linuxbrew
     set HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew"
     set HOMEBREW_CELLAR "/home/linuxbrew/.linuxbrew/Cellar"
     set HOMEBREW_REPOSITORY "/home/linuxbrew/.linuxbrew/Homebrew"
-    set PATH $PATH "/home/linuxbrew/.linuxbrew/bin"
-    set PATH $PATH "/home/linuxbrew/.linuxbrew/sbin"
+    set PATH "/home/linuxbrew/.linuxbrew/bin" $PATH
+    set PATH "/home/linuxbrew/.linuxbrew/sbin" $PATH
     set MANPATH $MANPATH "/home/linuxbrew/.linuxbrew/share/man"
     set INFOPATH $INFOPATH "/home/linuxbrew/.linuxbrew/share/info"
 end
