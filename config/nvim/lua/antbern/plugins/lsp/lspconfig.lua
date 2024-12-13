5370570e650587ee5f9dfafc5463d35a684237d4
antbern <40672068+antbern@users.noreply.github.com>
2024-12-13 17:44:26 +0100

    [nvim] format on save if LSP supports formatting

--- config/nvim/lua/antbern/plugins/lsp/lspconfig.lua
+++ config/nvim/lua/antbern/plugins/lsp/lspconfig.lua
@@ -76,6 +76,24 @@ return {
 			end
 		end
 
+		-- auto format on save if LSP supports
+		vim.api.nvim_create_autocmd('LspAttach', {
+			callback = function(args)
+				local client = vim.lsp.get_client_by_id(args.data.client_id)
+				if not client then return end
+
+				if client.supports_method('textDocument/formatting') then
+					-- Format the current buffer on save
+					vim.api.nvim_create_autocmd('BufWritePre', {
+						buffer = args.buf,
+						callback = function()
+							vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+						end
+					})
+				end
+			end
+		})
+
 		-- used to enable autocompletion (assign to every lsp server config)
 		local capabilities = cmp_nvim_lsp.default_capabilities()
 
