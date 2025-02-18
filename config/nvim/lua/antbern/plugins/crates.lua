0631d0702e2c65ae97289f83054c2bd33d5cd69a
antbern <40672068+antbern@users.noreply.github.com>
2025-02-18 16:53:22 +0100

    [nvim] improve config for crates.nvim

--- config/nvim/lua/antbern/plugins/crates.lua
+++ config/nvim/lua/antbern/plugins/crates.lua
@@ -2,19 +2,28 @@ return {
 	'saecki/crates.nvim',
 	tag = 'stable',
 	config = function()
-		require('crates').setup()
-
 		local crates = require("crates")
-		local opts = { silent = true }
+		crates.setup({
+			lsp = {
+				enabled = true,
+				on_attach = function(client, bufnr)
+					-- the same on_attach function as for your other lsp's
+					local opts = { silent = true, buffer = bufnr }
 
-		opts.desc = "Show versions popup (Crates)"
-		vim.keymap.set("n", "<leader>cv", crates.show_versions_popup, opts)
-		opts.desc = "Show features popup (Crates)"
-		vim.keymap.set("n", "<leader>cf", crates.show_features_popup, opts)
-		opts.desc = "Show dependencies popup (Crates)"
-		vim.keymap.set("n", "<leader>cd", crates.show_dependencies_popup, opts)
+					opts.desc = "Show versions popup (Crates)"
+					vim.keymap.set("n", "<leader>cv", crates.show_versions_popup, opts)
+					opts.desc = "Show features popup (Crates)"
+					vim.keymap.set("n", "<leader>cf", crates.show_features_popup, opts)
+					opts.desc = "Show dependencies popup (Crates)"
+					vim.keymap.set("n", "<leader>cd", crates.show_dependencies_popup, opts)
 
-		opts.desc = "Show popup (Crates)"
-		vim.keymap.set("n", "<leader>cd", crates.show_popup, opts)
+					opts.desc = "Show popup (Crates)"
+					vim.keymap.set("n", "<leader>cd", crates.show_popup, opts)
+				end,
+				actions = true,
+				completion = true,
+				hover = true,
+			}
+		})
 	end,
 }
